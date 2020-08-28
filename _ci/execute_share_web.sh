a2bd85107234fd188c62dadaefa6b8b1e1a4188a
Bogdan B <bogdan.bocancea@gmail.com>
2020-08-28 11:05:23 +0300

    APPS-472: Configure test stages to run on a single ACS instance

--- _ci/execute_share_web.sh
+++ _ci/execute_share_web.sh
@@ -13,22 +13,19 @@ git checkout 6.2.N
 export GROUP=$1
 mvn clean install \
                -DsuiteXmlFile='src/test/resources/share-po-runner-suite.xml' \
-               -Dalfresco.port=8080 \
                -Dalfresco.restApi.basicAuthScheme=true \
                -Djmx.useJolokiaAgent=true \
                -DincludeGroups=${GROUP} \
                -DexcludeGroups='google-docs,unit,SmartFolders,ExternalUsers,tobefixed,office,TransformationServer,xsstests' \
                -DrunBugs=false \
-               -Dalfresco.server=localhost \
-               -Dalfresco.host=localhost \
-               -Dalfresco.port=8080 \
-               -Dalfresco.url='http://localhost:8080/alfresco' \
-               -Dshare.host=localhost \
-               -Dshare.port=8181 \
-               -Dshare.url='http://localhost:8181/share' \
-               -Dalfresco.scheme=http \
+               -Dalfresco.server=csirbu.dev.alfresco.me \
+               -Dalfresco.host=csirbu.dev.alfresco.me \
+               -Dalfresco.port=443 \
+               -Dshare.host=csirbu.dev.alfresco.me \
+               -Dshare.port=443 \
+               -Dalfresco.scheme=https \
                -Dadmin.user=admin \
-               -Dadmin.password=admin \
+               -Dadmin.password=Alfresco!12346789 \
                -Dwebdriver.grid.url='http://127.0.0.1:4444/wd/hub' \
                -Dwebdriver.local.grid=false \
                -Dwebdriver.localGrid=false \
@@ -59,4 +56,3 @@ set +vex
 echo "=========================== Finishing Project Alfresco Tas Share =========================="
 
 exit ${SUCCESS}
-
